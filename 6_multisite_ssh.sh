e5c6e4b2724a11a16651da361b0edbd40a95cb93
Ajay Naidu Jami <52778264+AjayNaiduJami@users.noreply.github.com>
2019-10-10 20:25:10 +0530

    Basic modifications

--- 6_multisite_ssh.sh
+++ 6_multisite_ssh.sh
@@ -4,43 +4,43 @@ sudo apt-get update -y
 sudo apt-get install -y apache2
 sudo apt-get install -y awscli
 
-sudo mkdir -p /var/www/html/techtales.ml/public_html
-sudo mkdir -p /var/www/html/learncorp.ml/public_html
+sudo mkdir -p /var/www/html/qa.techtales.ml/public_html
+sudo mkdir -p /var/www/html/test.techtales.ml/public_html
 
 sudo chmod -R 755 /var/www/html/
 
-sudo touch /var/www/html/techtales.ml/public_html/index.html
-sudo cat > /var/www/html/techtales.ml/public_html/index.html<<EOF
+sudo touch /var/www/html/qa.techtales.ml/public_html/index.html
+sudo cat > /var/www/html/qa.techtales.ml/public_html/index.html<<EOF
 <html>
  <head>
- <title>www.techtales.ml</title>
+ <title>qa.techtales.ml</title>
  </head>
  <body>
- <h1>Hello, This is a test page for techtales.ml website</h1>
+ <h1>Hello, This is a test page for qa.techtales.ml website</h1>
  </body>
 </html>
 EOF
 
-sudo touch /var/www/html/learncorp.ml/public_html/index.html
-sudo cat > /var/www/html/learncorp.ml/public_html/index.html<<EOF
+sudo touch /var/www/html/test.techtales.ml/public_html/index.html
+sudo cat > /var/www/html/test.techtales.ml/public_html/index.html<<EOF
 <html>
  <head>
- <title>www.learncorp.ml</title>
+ <title>test.techtales.ml</title>
  </head>
  <body>
- <h1>Hello, This is a test page for learncorp.ml website</h1>
+ <h1>Hello, This is a test page for test.techtales.ml website</h1>
  </body>
 </html>
 EOF
 
-sudo touch /etc/apache2/sites-available/techtales.ml.conf
-sudo cat > /etc/apache2/sites-available/techtales.ml.conf<<EOF
+sudo touch /etc/apache2/sites-available/qa.techtales.ml.conf
+sudo cat > /etc/apache2/sites-available/qa.techtales.ml.conf<<EOF
 <VirtualHost *:80>
         
         ServerAdmin email@example.net
-        ServerName techtales.ml
-        ServerAlias www.techtales.ml
-        DocumentRoot /var/www/html/techtales.ml/public_html
+        ServerName qa.techtales.ml
+        ServerAlias qa..techtales.ml
+        DocumentRoot /var/www/html/qa.techtales.ml/public_html
 
         ErrorLog ${APACHE_LOG_DIR}/error.log
         CustomLog ${APACHE_LOG_DIR}/access.log combined
@@ -50,16 +50,16 @@ sudo cat > /etc/apache2/sites-available/techtales.ml.conf<<EOF
 # vim: syntax=apache ts=4 sw=4 sts=4 sr noet
 EOF
 
-sudo chmod 644 /etc/apache2/sites-available/techtales.ml.conf
+sudo chmod 644 /etc/apache2/sites-available/qa.techtales.ml.conf
 
-sudo touch /etc/apache2/sites-available/learncorp.ml.conf
-sudo cat > /etc/apache2/sites-available/learncorp.ml.conf<<EOF
+sudo touch /etc/apache2/sites-available/test.techtales.ml.conf
+sudo cat > /etc/apache2/sites-available/test.techtales.ml.conf<<EOF
 <VirtualHost *:80>
         
         ServerAdmin email@example.net
-        ServerName learncorp.ml
-        ServerAlias www.learncorp.ml
-        DocumentRoot /var/www/html/learncorp.ml/public_html
+        ServerName test.techtales.ml
+        ServerAlias test.techtales.ml
+        DocumentRoot /var/www/html/test.techtales.ml/public_html
 
         
         ErrorLog ${APACHE_LOG_DIR}/error.log
@@ -70,7 +70,7 @@ sudo cat > /etc/apache2/sites-available/learncorp.ml.conf<<EOF
 # vim: syntax=apache ts=4 sw=4 sts=4 sr noet
 EOF
 
-sudo chmod 644 /etc/apache2/sites-available/learncorp.ml.conf
+sudo chmod 644 /etc/apache2/sites-available/test.techtales.ml.conf
 
 echo "Prerequisites for configure Apache with SSL"
 
@@ -86,16 +86,16 @@ aws_access_key_id = *
 aws_secret_access_key = *
 EOF
 
-sudo mkdir -p /etc/apache2/ssl/techtales.ml
-sudo mkdir -p /etc/apache2/ssl/learncorp.ml
+sudo mkdir -p /etc/apache2/ssl/techtales.ml/
+sudo mkdir -p /etc/apache2/ssl/learncorp.ml/
 
 sudo aws s3 cp s3://techtales.ml/https/privkey.pem /etc/apache2/ssl/techtales.ml/.
 sudo aws s3 cp s3://techtales.ml/https/cert.pem /etc/apache2/ssl/techtales.ml/.
 sudo aws s3 cp s3://techtales.ml/https/chain.pem /etc/apache2/ssl/techtales.ml/.
 
-sudo aws s3 cp s3://learncorp.ml/https/privkey.pem /etc/apache2/ssl/learncorp.ml/.
-sudo aws s3 cp s3://learncorp.ml/https/cert.pem /etc/apache2/ssl/learncorp.ml/.
-sudo aws s3 cp s3://learncorp.ml/https/chain.pem /etc/apache2/ssl/learncorp.ml/.
+sudo aws s3 cp s3://test.techtales/https/privkey.pem /etc/apache2/ssl/learncorp.ml/.
+sudo aws s3 cp s3://test.techtales/https/cert.pem /etc/apache2/ssl/learncorp.ml/.
+sudo aws s3 cp s3://test.techtales/https/chain.pem /etc/apache2/ssl/learncorp.ml/.
 
 echo "Apache virtual host configuration"
 echo "Configuring Uncomplicated Firewall (UFW)"
@@ -106,15 +106,15 @@ sudo ufw allow 'Apache Full'
 sudo ufw allow 'OpenSSH'
 sudo ufw status
 
-sudo touch /etc/apache2/sites-available/techtales-ssl.conf
-sudo cat > /etc/apache2/sites-available/techtales-ssl.conf<<EOF
+sudo touch /etc/apache2/sites-available/qa.techtales-ssl.conf
+sudo cat > /etc/apache2/sites-available/qa.techtales-ssl.conf<<EOF
 <IfModule mod_ssl.c>
 	<VirtualHost _default_:443>
 		ServerAdmin email2@example.net
-		ServerName techtales.ml
-		ServerAlias www.techtales.ml
+		ServerName qa.techtales.ml
+		ServerAlias qa.techtales.ml
 
-		DocumentRoot /var/www/html/techtales.ml/public_html
+		DocumentRoot /var/www/html/qa.techtales.ml/public_html
 		
 		ErrorLog ${APACHE_LOG_DIR}/error.log
 		CustomLog ${APACHE_LOG_DIR}/access.log combined
@@ -135,26 +135,26 @@ sudo cat > /etc/apache2/sites-available/techtales-ssl.conf<<EOF
 </If
 EOF
 
-sudo chmod 644 /etc/apache2/sites-available/techtales-ssl.conf
+sudo chmod 644 /etc/apache2/sites-available/qa.techtales-ssl.conf
 
 
-sudo touch /etc/apache2/sites-available/learncorp-ssl.conf
-sudo cat > /etc/apache2/sites-available/learncorp-ssl.conf<<EOF
+sudo touch /etc/apache2/sites-available/test.techtales-ssl.conf
+sudo cat > /etc/apache2/sites-available/test.techtales-ssl.conf<<EOF
 <IfModule mod_ssl.c>
 	<VirtualHost _default_:443>
 		ServerAdmin email1@example.net
-		ServerName learncorp.ml
-		ServerAlias www.learncorp.ml
+		ServerName test.techtales.ml
+		ServerAlias test.techtales.ml
 
-		DocumentRoot /var/www/html/learncorp.ml/public_html
+		DocumentRoot /var/www/html/test.techtales.ml/public_html
 		
 		ErrorLog ${APACHE_LOG_DIR}/error.log
 		CustomLog ${APACHE_LOG_DIR}/access.log combined
 
 		SSLEngine on
 		
-		SSLCertificateFile	/etc/apache2/ssl/learncorp.ml/cert.pem
-		SSLCertificateKeyFile /etc/apache2/ssl/learncorp.ml/privkey.pem
+		SSLCertificateFile	/etc/apache2/ssl/techtales.ml/cert.pem
+		SSLCertificateKeyFile /etc/apache2/ssl/techtales.ml/privkey.pem
 		
 		<FilesMatch "\.(cgi|shtml|phtml|php)$">
 				SSLOptions +StdEnvVars
@@ -167,13 +167,13 @@ sudo cat > /etc/apache2/sites-available/learncorp-ssl.conf<<EOF
 </IfModule>
 EOF
 
-sudo chmod 644 /etc/apache2/sites-available/learncorp-ssl.conf
+sudo chmod 644 /etc/apache2/sites-available/test.techtales-ssl.conf
 
 echo "Enabling Apache HTTP sites"
 
 sudo a2dissite 000-default.conf
-sudo a2ensite techtales.ml.conf
-sudo a2ensite learncorp.ml.conf
+sudo a2ensite qa.techtales.ml.conf
+sudo a2ensite test.techtales.ml.conf
 
 sudo systemctl reload apache2
 
@@ -185,8 +185,8 @@ sudo a2enmod ssl
 systemctl restart apache2
 #To enable the site 
 sudo a2dissite default-ssl.conf
-sudo a2ensite techtales-ssl.conf
-sudo a2ensite learncorp-ssl.conf
+sudo a2ensite qa.techtales-ssl.conf
+sudo a2ensite test.techtales-ssl.conf
 
 
 sudo systemctl reload apache2
